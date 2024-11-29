bf1ae0e842c9f65151f35be9039f5b74260c5806
Liam Laidlaw <liamlaidlaw04@gmail.com>
2024-11-29 11:18:28 -0500

    added some chages to network.py and main.py

--- main.py
+++ main.py
@@ -6,10 +6,30 @@
 import torch
 import torchaudio.datasets
 import torchaudio
+from network import Network
 
 
 def main():
-    pass
+    device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+    )
+    print(f"Using {device} device")
+
+    num_linear_layers = 0
+    linear_width = 0
+    in_size = 0
+    out_size = 0 
+
+    print("Creating linear network")
+    linear_net = Network(num_linear_layers, linear_width, in_size, out_size)
+
+    
+
+
 
 
 if __name__ == '__main__':
--- network.py
+++ network.py
@@ -1,14 +1,23 @@
 # Filename: network.py
 # Author: Liam Laidlaw
 # Created: 11-25-2024
-# Description: 
+# Description: Full feedforward linear network with no convolutions or recurrent structures involved
 
 
 from torch import nn
 
 class Network(nn.module):
-
-    def __init__(self):
+    def __init__(self, num_linear_layers: int, hidden_size: int, in_size: int, out_size: int):
         super().__init__()
         # only linear layers
-        self.linear = []
+        self.linear = [nn.Linear(in_size, hidden_size)]
+        self.linear.extend([nn.Linear(hidden_size, hidden_size) for _ in range(num_linear_layers)])
+        self.linear.append(nn.Linear(hidden_size, out_size))
+        self.activation = nn.ReLU()
+
+    def forward(self, x):
+        for layer in self.linear:
+            x = layer(x)
+            x = self.activation(x)
+        return x
+        
